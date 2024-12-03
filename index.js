4d5f6c473487547a81800e95fb811d00bac64a94
Mehmet_Akif_Dundar <dundarm22@itu.edu.tr>
2024-12-03 14:41:11 +0300

    firebase_initialize added

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello world");
+import { Command } from "commander";
+import inquirer from "inquirer";
+
+const program = new Command();
+
+
+program
+    .name("Uniswap V2 DEX")
+    .description("Uniswap V2 DEX Simülasyonu")
+    .version("0.0.1");
+
+program 
+    .command("start-app <username>")
+    .description("Log in to app")
+        .action((name) => {
+            console.log("Hello " + name);
+            chooseOption();
+        });
+
+async function chooseOption() {
+    console.clear();
+
+    const answers = await inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "ne yapmak istersin kardaş la",
+            choices: [
+                { name: 'Uniswap V2 DEX Simülasyonu Başlat', value: 'start' },
+                { name: 'Versiyon Bilgisi', value: 'version' },
+                { name: 'Yardım', value: 'help' },
+                { name: 'Çıkış', value: 'exit' },
+            ] 
+        }
+    ]);
+
+    if (answers.action === 'start') {
+        console.log('Simülasyon başlatıldı...');
+        // Burada simülasyon işlemleri yapılabilir
+    } else if (answers.action === 'version') {
+        console.log('Versiyon: 1.0.0');
+    } else if (answers.action === 'help') {
+        program.help(); // Yardım komutunu göster
+    } else if (answers.action === 'exit') {
+        console.log('Çıkılıyor...');
+        process.exit(0); // Uygulamadan çık
+    }
+}
+
+program.parse();
---
+++ initializeDatabase.js
@@ -0,0 +1,19 @@
+import { readFile } from "fs/promises";
+import { ref, set } from "firebase/database";
+import { rdb } from "./firebase.js";
+
+async function initializeDataBase() {
+    try {
+        const data = await readFile("data.json", "utf-8");            //tek tırnak kullanımlarını düzelt sonra belki
+        const pool = JSON.parse(data);
+
+        await set(ref(rdb), pool);
+        console.log("Database has been initialized succesfuly");
+
+    } 
+    catch(err) {
+        console.error("Error ocurred while initialize database", err);
+    }     
+}
+
+export { initializeDataBase };
